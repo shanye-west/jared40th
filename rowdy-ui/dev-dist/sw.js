ea887f2b936dcbf0476ba455f3ea7cc723518f71
shanye-west <shanepeterson32@gmail.com>
2025-12-02 20:09:27 -0800

    feat: update circle sizes in ScoreInputCell for improved UI consistency

--- rowdy-ui/dev-dist/sw.js
+++ rowdy-ui/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qae4s0j8jrs"
+    "revision": "0.bmhbocgr27c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- rowdy-ui/src/routes/Match.tsx
+++ rowdy-ui/src/routes/Match.tsx
@@ -169,26 +169,26 @@ const ScoreInputCell = memo(function ScoreInputCell({
           {/* Outer circles for eagle+ (2+ under par) */}
           {circleCount >= 2 && (
             <div 
-              className="absolute rounded-full border-2 border-black"
-              style={{ width: '30px', height: '30px' }}
+              className="absolute rounded-full border border-black"
+              style={{ width: '32px', height: '32px' }}
             />
           )}
           {circleCount >= 3 && (
             <div 
-              className="absolute rounded-full border-2 border-black"
-              style={{ width: '34px', height: '34px' }}
+              className="absolute rounded-full border border-black"
+              style={{ width: '36px', height: '36px' }}
             />
           )}
           {circleCount >= 4 && (
             <div 
-              className="absolute rounded-full border-2 border-black"
-              style={{ width: '38px', height: '38px' }}
+              className="absolute rounded-full border border-black"
+              style={{ width: '40px', height: '40px' }}
             />
           )}
           {/* Inner circle for birdie (always shown when under par) */}
           <div 
-            className="absolute rounded-full border-2 border-black"
-            style={{ width: '26px', height: '26px' }}
+            className="absolute rounded-full border border-black"
+            style={{ width: '28px', height: '28px' }}
           />
         </div>
       )}
