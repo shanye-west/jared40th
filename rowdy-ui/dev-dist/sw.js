2c1a9d28041313acb3d6aae3f432aaf0d19bff0c
shanye-west <shanepeterson32@gmail.com>
2025-12-29 13:42:01 -0800

    fix: update service worker revision and adjust text size in Round component for better readability

--- rowdy-ui/dev-dist/sw.js
+++ rowdy-ui/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-0aa01d1d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.skaq6dbt41"
+    "revision": "0.bs2j78e2m0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
