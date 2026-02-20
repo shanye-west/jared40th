8ff768d528d33eac9f5a703308c54fcebc4a5b03
shanye-west <shanepeterson32@gmail.com>
2026-02-20 07:55:33 -0800

    updated home screen icon

--- jared40th-ui/vite.config.ts
+++ jared40th-ui/vite.config.ts
@@ -76,19 +76,19 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/jaredlogo.jpg',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/jpeg',
+            type: 'image/png',
           },
           {
-            src: '/jaredlogo.jpg',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/jpeg',
+            type: 'image/png',
           },
           {
-            src: '/jaredlogo.jpg',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/jpeg',
+            type: 'image/png',
             purpose: 'any maskable',
           },
         ],
